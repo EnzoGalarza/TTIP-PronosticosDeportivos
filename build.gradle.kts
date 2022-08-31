1b349b73aba3fcf2d710023d3e40ed28a56eeb81
EnzoGalarza <EnzoGl33@hotmail.com>
2022-08-31 18:01:47 -0300

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,70 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.7.3"
+	id("io.spring.dependency-management") version "1.0.13.RELEASE"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
+	jacoco
+}
+
+group = "ar.edu.unq.grupo7"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_14
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "14"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.test {
+	finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
+}
+
+jacoco {
+	toolVersion = "0.8.7"
+}
+
+tasks.jacocoTestReport {
+	classDirectories.setFrom(
+		files(classDirectories.files.map {
+			fileTree(it) {
+				exclude("**/config/**", "**/entity/**", "**/*Application*.*", "**/ServletInitializer.*")
+			}
+		})
+	)
+	reports {
+		xml.required.set(true)
+		csv.required.set(false)
+		html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+	}
+}
+
+tasks.register("runOnGitHub") {
+	dependsOn("jacocoTestReport")
+	group = "custom"
+	description = "$ ./gradlew runOnGitHub # runs on GitHub Action"
+}
---
+++ src/main/kotlin/ar/edu/unq/grupo7/PronosticosDeportivos/PronosticosDeportivosApplication.kt
@@ -0,0 +1,11 @@
+package ar.edu.unq.grupo7.PronosticosDeportivos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PronosticosDeportivosApplication
+
+fun main(args: Array<String>) {
+	runApplication<PronosticosDeportivosApplication>(*args)
+}
