36e89e7aaa3ee341f153e3ae4a193f3bbdef2d13
EnzoGalarza <EnzoGl33@hotmail.com>
2022-09-05 19:10:03 -0300

    Refactor en nombre de packages y path de los archivos

---
+++ src/main/kotlin/ar/edu/unq/grupo7/PronosticosDeportivos/model/Competition.kt
@@ -0,0 +1,3 @@
+package ar.edu.unq.grupo7.pronosticosDeportivos.model
+
+data class Competition(var name : String, var emblem : String, var code : String)
---
+++ src/main/kotlin/ar/edu/unq/grupo7/PronosticosDeportivos/service/CompetitionService.kt
@@ -0,0 +1,31 @@
+package ar.edu.unq.grupo7.PronosticosDeportivos.api.competition
+
+import ar.edu.unq.grupo7.PronosticosDeportivos.dto.CompetitionsDTO
+import ar.edu.unq.grupo7.PronosticosDeportivos.model.competition.Competition
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+
+@Service
+class CompetitionService {
+
+    @Autowired
+    lateinit var restTemplate : RestTemplate
+
+    fun getCompetitions() : MutableList<Competition>{
+        var headers : HttpHeaders = HttpHeaders()
+        headers.set("X-Auth-Token","f5bedce0ca024352a218d300e71d0798")
+        var entity = HttpEntity("parameters",headers)
+
+        var response = restTemplate.exchange("https://api.football-data.org/v4/competitions",HttpMethod.GET,entity,
+            object : ParameterizedTypeReference<CompetitionsDTO>() {}
+        )
+
+        return response.body!!.competitions
+    }
+
+}
