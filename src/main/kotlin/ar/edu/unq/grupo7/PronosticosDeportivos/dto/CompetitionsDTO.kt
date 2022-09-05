bfe2ee28f3c95d07f89b34e38693d9c0134ba904
EnzoGalarza <EnzoGl33@hotmail.com>
2022-09-05 18:11:54 -0300

    Agrego DTO de lista de competiciones

---
+++ src/main/kotlin/ar/edu/unq/grupo7/PronosticosDeportivos/dto/CompetitionsDTO.kt
@@ -0,0 +1,6 @@
+package ar.edu.unq.grupo7.PronosticosDeportivos.dto
+
+import ar.edu.unq.grupo7.PronosticosDeportivos.model.competition.Competition
+
+data class CompetitionsDTO(var competitions: MutableList<Competition>) {
+}
