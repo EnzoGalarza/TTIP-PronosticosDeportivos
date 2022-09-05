704308257dbbedc652fcb13d464059538f5968c8
EnzoGalarza <EnzoGl33@hotmail.com>
2022-09-05 18:11:29 -0300

    Agrego atributos a la clase Competition

--- src/main/kotlin/ar/edu/unq/grupo7/PronosticosDeportivos/model/competition/Competition.kt
+++ src/main/kotlin/ar/edu/unq/grupo7/PronosticosDeportivos/model/competition/Competition.kt
@@ -1,14 +1,4 @@
 package ar.edu.unq.grupo7.PronosticosDeportivos.model.competition
 
-class Competition {
-    var id: Long = 0
-    var name: String? = null
-
-    //Constructor
-    constructor() {}
-    constructor(id: Long, name: String?){
-        this.id = id
-        this.name = name
-    }
-
+data class Competition(var name : String, var emblem : String, var code : String) {
 }
