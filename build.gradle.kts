caadfc4e2caf9a038852121d976a581275d325f0
EnzoGalarza <EnzoGl33@hotmail.com>
2022-09-08 15:18:22 -0300

    Configuracion base de datos PostgreSQL

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 	jacoco
+	kotlin("plugin.jpa") version "1.5.21"
 }
 
 group = "ar.edu.unq.grupo7"
@@ -21,6 +22,8 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.postgresql:postgresql:42.5.0")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.3")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
