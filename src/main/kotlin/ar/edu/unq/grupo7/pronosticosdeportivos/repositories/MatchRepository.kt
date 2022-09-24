d52151e908fa9bf1d99de1520cee38d8dbc59077
EnzoGalarza <EnzoGl33@hotmail.com>
2022-09-24 00:19:03 -0300

    Agrego repository para persistir y recuperar Match y Competition

---
+++ src/main/kotlin/ar/edu/unq/grupo7/pronosticosdeportivos/repositories/MatchRepository.kt
@@ -0,0 +1,20 @@
+package ar.edu.unq.grupo7.pronosticosdeportivos.repositories
+
+import ar.edu.unq.grupo7.pronosticosdeportivos.model.Competition
+import ar.edu.unq.grupo7.pronosticosdeportivos.model.Match
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
+
+@Configuration
+@Repository
+interface MatchRepository : JpaRepository<Match, Long> {
+
+    @Query("SELECT COUNT(*) FROM partidos\n" +
+            "WHERE (competition = ?2 AND match_day = ?1 AND status = 'FINISHED')", nativeQuery = true)
+    fun countFinishedMatchesByCompetition(matchDay : Int, competition: String) : Int
+
+    @Query("SELECT * FROM partidos WHERE (match_day = ?1)", nativeQuery = true)
+    fun findByMatchDay(match_day: Int) : List<Match>
+}
