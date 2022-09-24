8a278705a59ac14c4ee906f738901278580a78ca
EnzoGalarza <EnzoGl33@hotmail.com>
2022-09-24 00:19:45 -0300

    Modifico service de Competition para persistir las competencias

--- src/main/kotlin/ar/edu/unq/grupo7/pronosticosdeportivos/service/CompetitionService.kt
+++ src/main/kotlin/ar/edu/unq/grupo7/pronosticosdeportivos/service/CompetitionService.kt
@@ -4,6 +4,7 @@ import ar.edu.unq.grupo7.pronosticosdeportivos.model.dto.CompetitionsDTO
 import ar.edu.unq.grupo7.pronosticosdeportivos.model.Competition
 import ar.edu.unq.grupo7.pronosticosdeportivos.model.dto.CurrentSeasonDTO
 import ar.edu.unq.grupo7.pronosticosdeportivos.model.dto.SeasonDTO
+import ar.edu.unq.grupo7.pronosticosdeportivos.repositories.CompetitionRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.HttpEntity
@@ -18,16 +19,25 @@ class CompetitionService {
     @Autowired
     lateinit var restTemplate : RestTemplate
 
+    @Autowired
+    lateinit var repository : CompetitionRepository
+
     fun getCompetitions() : MutableList<Competition>{
-        var headers : HttpHeaders = HttpHeaders()
-        headers.set("X-Auth-Token","f5bedce0ca024352a218d300e71d0798")
-        var entity = HttpEntity("parameters",headers)
+        if(repository.count() == 0L){
+            var headers : HttpHeaders = HttpHeaders()
+            headers.set("X-Auth-Token","f5bedce0ca024352a218d300e71d0798")
+            var entity = HttpEntity("parameters",headers)
 
-        var response = restTemplate.exchange("https://api.football-data.org/v4/competitions",HttpMethod.GET,entity,
-            object : ParameterizedTypeReference<CompetitionsDTO>() {}
-        )
+            var response = restTemplate.exchange("https://api.football-data.org/v4/competitions",HttpMethod.GET,entity,
+                object : ParameterizedTypeReference<CompetitionsDTO>() {}
+            )
+            val competitions = response.body!!.competitions
 
-        return response.body!!.competitions
+            repository.saveAll(competitions)
+            return competitions
+        }
+
+        return repository.findAll()
     }
 
     fun getCurrentMatchDay(competition: String) : Int{
