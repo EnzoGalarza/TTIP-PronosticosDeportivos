d589083a304400e52823a022a1e304178f92c0de
EnzoGalarza <EnzoGl33@hotmail.com>
2022-09-24 00:16:44 -0300

    Agrego entidad Team y metodo toDTO y toModel de los equipos

---
+++ src/main/kotlin/ar/edu/unq/grupo7/pronosticosdeportivos/model/Team.kt
@@ -0,0 +1,17 @@
+package ar.edu.unq.grupo7.pronosticosdeportivos.model
+
+import ar.edu.unq.grupo7.pronosticosdeportivos.model.dto.TeamDTO
+import javax.persistence.*
+
+@Entity
+@Table(name = "equipos")
+class Team(val name: String, val tla : String?, val crest : String?) {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id : Long = 0
+
+
+}
+
+fun Team.toDTO() = TeamDTO(name = name, tla = tla, crest = crest)
--- src/main/kotlin/ar/edu/unq/grupo7/pronosticosdeportivos/model/dto/TeamDTO.kt
+++ src/main/kotlin/ar/edu/unq/grupo7/pronosticosdeportivos/model/dto/TeamDTO.kt
@@ -1,5 +1,9 @@
 package ar.edu.unq.grupo7.pronosticosdeportivos.model.dto
 
+import ar.edu.unq.grupo7.pronosticosdeportivos.model.Team
+
 data class TeamDTO(val name: String?, val tla: String?, val crest : String?) {
 
 }
+
+fun TeamDTO.toModel() = Team(name = name!!,tla = tla,crest = crest)
