82e21f0b449c0d965935f61c0cb28e1b0e7c9392
EnzoGalarza <EnzoGl33@hotmail.com>
2022-09-24 00:12:09 -0300

    Agrego entidad de Match, y metodos toDTO y toModel de los partidos

---
+++ src/main/kotlin/ar/edu/unq/grupo7/pronosticosdeportivos/model/Match.kt
@@ -0,0 +1,26 @@
+package ar.edu.unq.grupo7.pronosticosdeportivos.model
+
+import ar.edu.unq.grupo7.pronosticosdeportivos.model.dto.MatchDTO
+import ar.edu.unq.grupo7.pronosticosdeportivos.model.dto.ResultDTO
+import ar.edu.unq.grupo7.pronosticosdeportivos.model.dto.ScoreDTO
+import java.time.LocalDateTime
+import javax.persistence.*
+
+
+@Entity
+@Table(name = "partidos")
+class Match(@OneToMany(cascade = [CascadeType.ALL])
+            var teams : List<Team>, @Column var date : LocalDateTime, @Column var localGoals : Int?,
+            @Column var awayGoals : Int?, @Column val code : Long, @Column var status : String,
+            @Column var matchDay : Int, @Column var competition : String) {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id : Long = 0
+
+}
+
+fun Match.toDTO() = MatchDTO(id = code, status = status, utcDate = date, matchday = matchDay,homeTeam = teams[0].toDTO(),
+awayTeam = teams[1].toDTO(), score = ScoreDTO("",ResultDTO(localGoals,awayGoals))
+)
+
--- src/main/kotlin/ar/edu/unq/grupo7/pronosticosdeportivos/model/dto/MatchDTO.kt
+++ src/main/kotlin/ar/edu/unq/grupo7/pronosticosdeportivos/model/dto/MatchDTO.kt
@@ -1,5 +1,18 @@
 package ar.edu.unq.grupo7.pronosticosdeportivos.model.dto
 
-data class MatchDTO(var id : Long, var status: String, var utcDate : String, val matchday: Int, var homeTeam : TeamDTO, var awayTeam : TeamDTO, var score : ScoreDTO) {
+import ar.edu.unq.grupo7.pronosticosdeportivos.model.Match
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
+data class MatchDTO(var id : Long, var status: String, var utcDate : LocalDateTime, val matchday: Int, var homeTeam : TeamDTO, var awayTeam : TeamDTO, var score : ScoreDTO, var localDate : String = "") {
+
+    init {
+        val offSet = utcDate.atZone(ZoneId.systemDefault()).offset
+        localDate = utcDate.atOffset(ZoneOffset.UTC).withOffsetSameInstant(offSet).format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"))
+    }
 }
+
+fun MatchDTO.toModel(competition : String) = Match(teams = listOf(homeTeam.toModel(),awayTeam.toModel()),date = utcDate,
+    localGoals = score.fullTime.home, awayGoals = score.fullTime.away, code = id,status = status, matchDay = matchday, competition = competition)
