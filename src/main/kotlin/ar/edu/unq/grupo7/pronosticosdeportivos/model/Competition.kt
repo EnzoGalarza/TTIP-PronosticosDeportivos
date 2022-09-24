9b0d127c2029c2fbabef8cd13769a1661f48c406
EnzoGalarza <EnzoGl33@hotmail.com>
2022-09-24 00:17:09 -0300

    Modifico competition para persistir entidad

--- src/main/kotlin/ar/edu/unq/grupo7/pronosticosdeportivos/model/Competition.kt
+++ src/main/kotlin/ar/edu/unq/grupo7/pronosticosdeportivos/model/Competition.kt
@@ -1,3 +1,12 @@
 package ar.edu.unq.grupo7.pronosticosdeportivos.model
 
-data class Competition(var name : String, var emblem : String, var code : String)
+import javax.persistence.*
+
+@Entity
+@Table(name = "competencias")
+data class Competition(var name : String, var emblem : String, var code : String){
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id : Long = 0
+}
