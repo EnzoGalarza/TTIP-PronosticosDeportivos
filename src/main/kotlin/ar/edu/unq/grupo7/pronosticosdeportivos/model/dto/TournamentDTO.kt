2d1dcd36412b88823252936f546baac734699266
EnzoGalarza <EnzoGl33@hotmail.com>
2022-10-19 22:25:48 -0300

    agrego atributos en el torneo dto

--- src/main/kotlin/ar/edu/unq/grupo7/pronosticosdeportivos/model/dto/TournamentDTO.kt
+++ src/main/kotlin/ar/edu/unq/grupo7/pronosticosdeportivos/model/dto/TournamentDTO.kt
@@ -1,7 +1,8 @@
 package ar.edu.unq.grupo7.pronosticosdeportivos.model.dto
 
+import ar.edu.unq.grupo7.pronosticosdeportivos.model.tournaments.Criteria
 import ar.edu.unq.grupo7.pronosticosdeportivos.model.tournaments.Tournament
 
-data class TournamentDTO(val name : String, val competition : String, val usersEmail : List<String>)
+data class TournamentDTO(val name : String, val competition : String, val creator : String, val usersEmail : List<String>)
 
-fun TournamentDTO.toModel() = Tournament(name = name, competition = competition)
+fun TournamentDTO.toModel() = Tournament(name = name, competition = competition, listOf(Criteria("Complete",3)))
