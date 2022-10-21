14997795d945e70fa68786420ac9dc7ee14bb575
Fede <fedejmartinez@gmail.com>
2022-10-21 13:20:24 -0300

    Update criteria

--- src/main/kotlin/ar/edu/unq/grupo7/pronosticosdeportivos/model/dto/TournamentDTO.kt
+++ src/main/kotlin/ar/edu/unq/grupo7/pronosticosdeportivos/model/dto/TournamentDTO.kt
@@ -3,6 +3,7 @@ package ar.edu.unq.grupo7.pronosticosdeportivos.model.dto
 import ar.edu.unq.grupo7.pronosticosdeportivos.model.tournaments.Criteria
 import ar.edu.unq.grupo7.pronosticosdeportivos.model.tournaments.Tournament
 
-data class TournamentDTO(val name : String, val competition : String, val creator : String, val usersEmail : List<String>)
+data class TournamentDTO(val name : String, val competition : String, val creator : String, val usersEmail : List<String>,
+                         val criteria : List<Criteria>)
 
 fun TournamentDTO.toModel() = Tournament(name = name, competition = competition, listOf(Criteria("Complete",3)))
