dbdf915343625177d83af47cadf49a44d6dc0dfe
Fede <fedejmartinez@gmail.com>
2022-11-11 21:22:21 -0300

    Agrega dias de la semana

--- src/main/kotlin/ar/edu/unq/grupo7/pronosticosdeportivos/model/dto/MatchDTO.kt
+++ src/main/kotlin/ar/edu/unq/grupo7/pronosticosdeportivos/model/dto/MatchDTO.kt
@@ -2,16 +2,18 @@ package ar.edu.unq.grupo7.pronosticosdeportivos.model.dto
 
 import ar.edu.unq.grupo7.pronosticosdeportivos.model.competitions.Match
 import ar.edu.unq.grupo7.pronosticosdeportivos.model.competitions.Team
+import java.time.DayOfWeek
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 
-data class MatchDTO(var id : Long, var status: String, var utcDate : LocalDateTime, val matchday: Int, var homeTeam : TeamDTO, var awayTeam : TeamDTO, var score : ScoreDTO, var localDate : String = "") {
+data class MatchDTO(var id : Long, var status: String, var utcDate : LocalDateTime, val matchday: Int, var homeTeam : TeamDTO, var awayTeam : TeamDTO, var score : ScoreDTO, var localDate : String = "", var weekDay : DayOfWeek? = null) {
 
     init {
         val offSet = utcDate.atZone(ZoneId.systemDefault()).offset
         localDate = utcDate.atOffset(ZoneOffset.UTC).withOffsetSameInstant(offSet).format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"))
+        weekDay = utcDate.atOffset(ZoneOffset.UTC).withOffsetSameInstant(offSet).dayOfWeek
     }
 }
 
